a1fc139e21b5943a299db8ecb41072b37350457e
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-12-13 00:07:08 -0300

    :tada: Initial Commit RestAPI with Swagger and Postgres

---
+++ rest-api-swagger/src/test/java/ferreira/restapiswagger/RestApiSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package ferreira.restapiswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
