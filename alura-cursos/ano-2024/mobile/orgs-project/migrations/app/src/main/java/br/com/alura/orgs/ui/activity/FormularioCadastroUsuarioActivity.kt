deb8650f646e3a040b687bf02a66c938793ca483
Lucas Ferreira <lks_ferreira@hotmail.com>
2024-07-24 17:17:09 -0300

    :tada: Iniciado novo módulo da formação

---
+++ alura-cursos/ano-2024/mobile/orgs-project/migrations/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioCadastroUsuarioActivity.kt
@@ -0,0 +1,54 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.databinding.ActivityFormularioCadastroUsuarioBinding
+import br.com.alura.orgs.model.Usuario
+
+class FormularioCadastroUsuarioActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFormularioCadastroUsuarioBinding.inflate(layoutInflater)
+    }
+
+    private val dao by lazy {
+        AppDatabase.instancia(this).usuarioDao()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraBotaoCadastrar()
+    }
+
+    private fun configuraBotaoCadastrar() {
+        binding.activityFormularioCadastroBotaoCadastrar.setOnClickListener {
+            val novoUsuario = criaUsuario()
+            Log.i("CadastroUsuario", "onCreate: $novoUsuario")
+            lifecycleScope.launchWhenCreated {
+                try {
+                    dao.salva(novoUsuario)
+                    finish()
+                } catch (e: Exception) {
+                    Log.e("CadastroUsuario", "onCreate: $e")
+                    Toast.makeText(
+                        this@FormularioCadastroUsuarioActivity,
+                        "Erro ao cadastrar usuário",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun criaUsuario(): Usuario {
+        val usuario = binding.activityFormularioCadastroUsuario.text.toString()
+        val nome = binding.activityFormularioCadastroNome.text.toString()
+        val senha = binding.activityFormularioCadastroSenha.text.toString()
+        return Usuario(usuario, nome, senha)
+    }
+}
