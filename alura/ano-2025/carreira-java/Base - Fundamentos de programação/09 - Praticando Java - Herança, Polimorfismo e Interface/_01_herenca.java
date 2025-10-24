f0021d93936d735e0d20f6c2a32b4580d04e95aa
Lucas Ferreira <lks_ferreira@hotmail.com>
2025-10-23 21:12:17 -0300

    ✨ feat: Adicionado conteúdo sobre herança em `java`

---
+++ alura/ano-2025/carreira-java/Base - Fundamentos de programação/09 - Praticando Java - Herança, Polimorfismo e Interface/_01_herenca.java
@@ -0,0 +1,74 @@
+public class _01_herenca {
+    public static void main(String[] args) {
+
+        Gerente gerente = new Gerente("João", 5000);
+        System.out.println(gerente.toString());
+
+        gerente.reajustaSalario(10);
+        System.out.println("Após reajuste:");
+        System.out.println(gerente.toString());
+
+        System.out.println("=== Desenvolvedor ===");
+        Desenvolvedor dev = new Desenvolvedor("Lucas", 4000, "Java");
+        System.out.println(dev.toString());
+        dev.reajustaSalario(10);
+        System.out.println("Após reajuste:");
+        System.out.println(dev.toString()); 
+
+
+    }
+
+    public static class Desenvolvedor extends Funcionario {
+
+        private String stack;
+
+        public Desenvolvedor(String nome, double salario, String stack) {
+            super(nome, salario);
+            this.stack = stack;
+        }        
+    }
+
+    public static class Gerente extends Funcionario {
+
+        private double bonus;
+
+        public Gerente(String nome, double salario) {
+            super(nome, salario);
+        }
+
+        public double getBonus() {
+            return bonus;
+        }
+
+        public void setBonus(double bonus) {
+            this.bonus = bonus;
+        }
+
+    }
+
+    public static class Funcionario {
+        private String nome;
+        private double salario;
+
+        public Funcionario(String nome, double salario) {
+            this.nome = nome;
+            this.salario = salario;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void reajustaSalario(double percentual) {
+            this.salario += this.salario * percentual / 100;
+        }
+
+        @Override
+        public String toString() {
+            return "Funcionario{" +
+                    "nome='" + nome + '\'' +
+                    ", salario=" + salario +
+                    '}';
+        }
+    }
+}
