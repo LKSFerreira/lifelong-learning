bd9d4dda0c5302337d8c2e34c36a2468eef040d5
Lucas Ferreira <lks_ferreira@hotmail.com>
2025-11-06 23:02:23 -0300

    ♻️ Trocado class para public static class

--- alura/ano-2025/carreira-java/Base - Fundamentos de programação/09 - Praticando Java - Herança, Polimorfismo e Interface/_05_sistema_escolar.java
+++ alura/ano-2025/carreira-java/Base - Fundamentos de programação/09 - Praticando Java - Herança, Polimorfismo e Interface/_05_sistema_escolar.java
@@ -1,50 +1,3 @@
-class Pessoa {
-    private String nome;
-    private int idade;
-
-    public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    public void exibirInfo() {
-        System.out.print("Nome: " + nome + ", Idade: " + idade);
-    }
-
-}
-
-class Aluno extends Pessoa {
-    private double nota;
-
-    public Aluno(String nome, int idade, double nota) {
-        super(nome, idade);
-        this.nota = nota;
-    }
-
-    @Override
-    public void exibirInfo() {
-        super.exibirInfo();
-        System.out.println(", Nota: " + nota);
-    }
-
-}
-
-class Docente extends Pessoa {
-    private String disciplina;
-
-    public Docente(String nome, int idade, String disciplina) {
-        super(nome, idade);
-        this.disciplina = disciplina;
-    }
-
-    @Override
-    public void exibirInfo() {
-        super.exibirInfo();
-        System.out.println(", Disciplina: " + disciplina);
-    }
-
-}
-
 public class _05_sistema_escolar {
     public static void main(String[] args) {
 
@@ -53,6 +6,53 @@ public class _05_sistema_escolar {
 
         Docente docente = new Docente("Lucas", 35, "Física");
         docente.exibirInfo();
+
+    }
+
+    public static class Pessoa {
+        private String nome;
+        private int idade;
+
+        public Pessoa(String nome, int idade) {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public void exibirInfo() {
+            System.out.print("Nome: " + nome + ", Idade: " + idade);
+        }
+
+    }
+
+    public static class Aluno extends Pessoa {
+        private double nota;
+
+        public Aluno(String nome, int idade, double nota) {
+            super(nome, idade);
+            this.nota = nota;
+        }
+
+        @Override
+        public void exibirInfo() {
+            super.exibirInfo();
+            System.out.println(", Nota: " + nota);
+        }
+
+    }
+
+    public static class Docente extends Pessoa {
+        private String disciplina;
+
+        public Docente(String nome, int idade, String disciplina) {
+            super(nome, idade);
+            this.disciplina = disciplina;
+        }
+
+        @Override
+        public void exibirInfo() {
+            super.exibirInfo();
+            System.out.println(", Disciplina: " + disciplina);
+        }
 
     }
 
