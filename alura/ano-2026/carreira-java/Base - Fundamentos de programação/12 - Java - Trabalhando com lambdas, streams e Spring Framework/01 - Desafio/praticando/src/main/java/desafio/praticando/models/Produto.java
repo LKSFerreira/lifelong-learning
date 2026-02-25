4aca32c7636e2809e7da2ccd889a8ef35dcf56da
Lucas Ferreira <lks_ferreira@hotmail.com>
2026-02-24 22:13:54 -0300

    :recycle: refactor: Adição de categoria e construtores na classe Produto

--- alura/ano-2026/carreira-java/Base - Fundamentos de programação/12 - Java - Trabalhando com lambdas, streams e Spring Framework/01 - Desafio/praticando/src/main/java/desafio/praticando/models/Produto.java
+++ alura/ano-2026/carreira-java/Base - Fundamentos de programação/12 - Java - Trabalhando com lambdas, streams e Spring Framework/01 - Desafio/praticando/src/main/java/desafio/praticando/models/Produto.java
@@ -3,6 +3,16 @@ package desafio.praticando.models;
 public class Produto {
     private String nome;
     private double preco;
+    private String categoria;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, double preco, String categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
 
     public String getNome() {
         return nome;
@@ -20,8 +30,16 @@ public class Produto {
         this.preco = preco;
     }
 
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
     @Override
     public String toString() {
-        return "Produto [nome=" + nome + ", preco=" + preco + "]";
+        return "Produto [nome=" + nome + ", preco=" + preco + ", categoria=" + categoria + "]";
     }
 }
